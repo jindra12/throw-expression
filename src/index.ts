94555d56375614949d8084669efb00c7e4858560
Jan Jindráček <jindra12.underdark@gmail.com>
2020-09-20 11:53:38 +0200

    Add js doc, publish 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/**
+ * Throws an error when called
+ * @param error either error object or a string
+ */
 export const Throw = (error: string | Error): never => {
     if (typeof error === 'string') {
         throw new Error(error);
@@ -5,6 +9,23 @@ export const Throw = (error: string | Error): never => {
     throw error;
 };
 
+/**
+ * Throw exception if value is null
+ * @param value value to be checked for null
+ * @param error either error object or a string
+ */
 export const throwIfNull = <T>(value: T | null, error: string | Error = 'value is null'): T => value === null ? Throw(error) : value;
+
+/**
+ * Throw exception if value is undefined
+ * @param value value to be checked for undefined
+ * @param error either error object or a string
+ */
 export const throwIfUndefined = <T>(value: T | undefined, error: string | Error = 'value is undefined'): T => value === undefined ? Throw(error) : value;
+
+/**
+ * Throw exception if value is null or undefined
+ * @param value value to be checked for null or undefined
+ * @param error either error object or a string
+ */
 export const throwIfFalsy = <T>(value: T, error: string | Error = 'value is undefined'): NonNullable<T> => (value === undefined || value === null) ? Throw(error) : value!;
